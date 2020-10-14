9b3e6a56c41865cf95f899982f85710ddf23b120
Andrew <acasta4704@gmail.com>
2020-10-14 17:58:44 +1100

    added routes for register, login & logout. Connected with pages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from cs50 import SQL
 from flask import g, Flask, flash, redirect, render_template, request, session, url_for
 from flask_session import Session
 from functools import wraps
+from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 
@@ -21,16 +22,14 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_FILE_DIR"] = mkdtemp() # prevents session data staying on server
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-# hardcoded user
-
 
 # TODO
 # validate /add form data
-# check wine_id from front end works
+# apply the login_require decorator to routes
 
 
 #################
@@ -42,20 +41,118 @@ Session(app)
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('login', next=request.url))
+        if session.get("user_id") is None:
+            return redirect("/login")
         return f(*args, **kwargs)
     return decorated_function
+
+# --------
+# Register
+# --------
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    #server register page
+    if request.method == "GET":
+        return render_template('register.html')
+
+    # process user registration
+    if request.method == "POST":
+        error = None
+        # get username and passwords from form
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_check = request.form.get("password-check")
+
+        # validate inputs
+        # are not blank
+        if not username or not password or not password_check:
+            error = 'please enter all fields'
+        # check passwords match
+        if password != password_check:
+            error = 'passwords did not match'
+
+        # check username doesn't exist in db
+        userCheck = db.execute('SELECT * FROM users WHERE username = ?', username)
+        if len(userCheck) > 0:
+            error = 'username already taken'
+
+        # else register all good, proceed
+        else:
+            # insert new user into db (store username & password hash)        
+            user_id = db.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username, generate_password_hash(password))
+            # log user in
+            session["user_id"] = user_id
+            # redirect to index
+            return redirect(url_for('index'))
+
+        # redirect with error if failed
+        return render_template('register.html', error=error)
+
+# --------
+# Login
+# --------
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    # serve login page
+    if request.method == "GET":
+        return render_template('login.html')
+
+    # process login & redirect to index
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        # validate inputs
+        # username & password inputted
+        if not username or not password:
+            error = 'please enter username & password'
+
+        # search DB for user_id
+        user = db.execute("SELECT id, username, hash FROM users WHERE username = ?", username)
+        # check username returns ONE results (if none, username doesn't exist)
+        # check password HASH is same as db hash
+        if len(user) != 1 or not check_password_hash(user[0]['hash'], password):
+            error = 'username or password incorrect'
+        # store user_id as session["user_id"] & redirect
+        else:
+            session["user_id"] = user[0]['id']
+            return redirect(url_for('index'))
+        
+        # if not match, return error message
+        return render_template('login.html', error=error)
+
+# --------
+# Logout
+# --------
+
+@app.route("/logout")
+@login_required
+def logout():
+    # Forget any user_id
+    session.clear()
+    # redirect to login
+    return url_for('index')
 
 #################
 # Frontend routes
 #################
 
-
+# ----
+# Index (home)
+# ----
 @app.route("/")
+@login_required
 def index():
-    session["user_id"] = 1
     return render_template('index.html', user_id=session["user_id"])
+
+# ----
+# Add
+# ----
 
 # add new review
 def allowed_file(filename):
@@ -67,7 +164,7 @@ def add():
     if request.method == "GET":
         return render_template('add.html')
     if request.method == "POST":
-        # hack user 1 for testing
+        # hack user 1 for testing ===========================================================<<<< TODO
         session["user_id"] = 1
         # collect all form data
         brand = request.form.get("brand")
@@ -79,7 +176,7 @@ def add():
         wine_id = request.form.get("wine_id")
         # needs to implement autocomplete, then check for wine_id if a match was found 
 
-        # TODO validate all form data
+        # validate all form data ===========================================================<<<< TODO
         
         # image
         # if image file provided, store in server folder & set image as location in db
@@ -100,9 +197,26 @@ def add():
 
         return redirect(url_for('add'))
 
+
+# ----
+# List
+# ----
+
+# ------
+# Detail
+# ------
+
+# ----
+# Edit
+# ----
+
 #############
 # API routes
 #############
+
+# -----
+# Wines
+# -----
 
 # wines lookup (return wines to frontend for lookup & return to DB via add route)
 @app.route('/wines')
@@ -111,14 +225,5 @@ def get_wines():
     data["wines"] = db.execute("SELECT * FROM wines")
     return data
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-# review lookup (for editing)
 
 
