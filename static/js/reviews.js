b75fa03bcf6ba2c0c53ff4e9acaddee323176ecb
Andrew <acasta4704@gmail.com>
2020-10-17 13:10:07 +1100

    data loaded coming through to interface

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -25,6 +25,7 @@ const drinkAgain = ""; // True | ''
 //---------
 
 // call load data function
+loadReviews();
 
 //------------
 // page events
@@ -58,40 +59,39 @@ const drinkAgain = ""; // True | ''
 // Helper
 // load reviews on page
 
-// get data from API (sort, filter, order)
-$.get(`/api/reviews?sort=${sort}&drink_again=${drinkAgain}&order=${order}`, reviews => {
-  // Build the HTML for each review
-  let reviewHTML = reviews.map(review => {
-    // Build star rating based on stored rating value
-    let starHTML = "";
-    for (let i = 0; i < 5; i++) {
-      if (review.rating > i) {
-        starHTML += '<span class="fa fa-star checked"></span>';
-      } else {
-        starHTML += '<span class="fa fa-star"></span>';
+function loadReviews() {
+  // get data from API (sort, filter, order)
+  $.get(`/api/reviews?sort=${sort}&drink_again=${drinkAgain}&order=${order}`, reviews => {
+    // Build the HTML for each review
+    let reviewHTML = reviews.map(review => {
+      // Build star rating based on stored rating value
+      let starHTML = "";
+      for (let i = 0; i < 5; i++) {
+        if (review.rating > i) {
+          starHTML += '<span class="fa fa-star checked"></span>';
+        } else {
+          starHTML += '<span class="fa fa-star"></span>';
+        }
       }
-    }
-    // return each review HTML article
-    return `<article class="review">
-    <h4 class="brand">${review.brand}</h4>
-    <div class="star-scale">
-    ${starHTML}
-    </div>
-    <img src="${review.image ? review.image : ""}" alt="image of review" class="review-img" />
-    <p><span class="variety">${review.variety}</span>, <span class="year">${review.year}</span></p>
-    <p class="datetime">${review.datetime}</p>
-    </article>`;
+      // return each review HTML article
+      return `<article class="review">
+      <h4 class="brand">${review.brand}</h4>
+      <div class="star-scale">
+      ${starHTML}
+      </div>
+      <div class="review-img-container">
+        <img src="${review.image ? review.image : ""}" alt="image of review" class="review-img" />
+      </div>
+      <p class="variety-year"><span class="variety">${review.variety}</span>, <span class="year">${
+        review.year
+      }</span></p>
+      <p class="datetime">${review.datetime}</p>
+      </article>`;
+    });
+    // join articles & insert data
+    reviewsHTML.innerHTML = reviewHTML.join("");
   });
-  // join articles & insert data
-  reviewsHTML.innerHTML = reviewHTML.join("");
-});
-
-// Html builder:
-// update DOM (reduce array, join, innerhtml)
-
-// Review star values
-// Loop over stars
-// check each v rating, if condition add .checked
+}
 
 /* ================
 // click review //
